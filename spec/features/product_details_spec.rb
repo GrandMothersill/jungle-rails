ad0b19c71b894018ae095f6ebca60488a9c43182
Graham Mothersill <graham.mothersill@gmail.com>
2020-08-27 23:21:07 +0000

    rspec test with capybara testing access of product page

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
-RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
+RSpec.feature "Visitor can navigate from the home page to the product detail page by clicking on a product", type: :feature, js: true do
 
- # SETUP
- before :each do
+  # SETUP
+  before :each do
   @category = Category.create! name: 'Apparel'
 
   10.times do |n|
@@ -17,15 +17,17 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
   end
 end
 
-  scenario "They see all products" do
+  scenario "they see one product in detail" do
     # ACT
     visit root_path
-
-
+    # visit ('about#show')
+    
+    click_link('Details', match: :first)
+    
     # DEBUG / VERIFY
+    
+    expect(page).to have_css 'article.product-detail', count: 1
     save_screenshot
-
-    expect(page).to have_css 'article.product', count: 10
   end
 
 end
